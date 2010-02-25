dae3e4fb313f46803621f2dfc9e0b7084a38ace5
maoe <maoe.maoe@gmail.com>
2010-02-25 14:42:16 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.maoe/fn-keyword "0.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]
+                     [leiningen/lein-swank "1.1.0"]])
---
+++ src/org/clojars/maoe/fn_keyword.clj
@@ -0,0 +1,38 @@
+(ns org.clojars.maoe.fn-keyword
+  (:gen-class))
+
+(defn- take-while-nth [n pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (when (pred (first s))
+       (concat (take n s)
+               (take-while-nth n pred (drop n s)))))))
+
+(defn- drop-while-nth [n pred coll]
+  (lazy-seq
+   (loop [p pred c coll]
+     (when-let [s (seq c)]
+       (if (p (first s))
+         (recur p (drop n s))
+         s)))))
+
+(defn- split-with-nth [n pred coll]
+  [(take-while-nth n pred coll)
+   (drop-while-nth n pred coll)])
+
+(defmacro defnk* [fn-name & fn-tail]
+  (let [[args & body]          fn-tail
+        [req-args kv-and-rest] (split-with symbol? args)
+        [key-vals extras]      (split-with-nth 2 keyword? kv-and-rest)
+        syms                   (map #(symbol (name %))
+                                    (take-nth 2 key-vals))
+        vals                   (take-nth 2 (rest key-vals))
+        sym-vals               (apply hash-map (interleave syms vals))
+        de-map                 {:keys (vec syms) :or sym-vals}
+        extras                 (nth extras 1)]
+    `(defn ~fn-name
+       [~@req-args & options#]
+       (let [[key-vals# extras#] (split-with-nth 2 keyword? options#)
+             ~de-map (apply hash-map key-vals#)
+             ~extras extras#]
+         ~@body))))
