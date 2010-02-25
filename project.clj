57a6c2970feec22eb8ae83526e394ecff1f3c43e
maoe <maoe.maoe@gmail.com>
2010-02-25 14:45:19 +0900

    add autodoc

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]
-                     [leiningen/lein-swank "1.1.0"]])
+                     [leiningen/lein-swank "1.1.0"]
+                     [autodoc "0.7.0"]])
