3ff550481aa270beb6bcb8f50aa33495502c696f
maoe <maoe.maoe@gmail.com>
2010-03-02 19:41:31 +0900

    remove swank-clojure from :dev-dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://github.com/maoe/funky"
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.0"]
-                     [leiningen/lein-swank "1.1.0"]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [autodoc "0.7.0"]]
   :repositories [["clojars" "http://clojars.org/repo"]])
