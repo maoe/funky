a463b410ae157f216af490cac8fd2b82a3f22d18
maoe <maoe.maoe@gmail.com>
2010-03-03 19:36:45 +0900

    more tests

--- test/test.clj
+++ test/test.clj
@@ -3,14 +3,10 @@
   (:use clojure.test))
 
 (deftest test-fnk
-  (let [f (fnk [a]
-            (list a))
-        g (fnk [:a 0]
-            (list a))
-        h (fnk [a :b "b"]
-            (list a b))
-        i (fnk [a :b "b" & c]
-            (list a b c))]
+  (let [f (fnk [a] (list a))
+        g (fnk [:a 0] (list a))
+        h (fnk [a :b "b"] (list a b))
+        i (fnk [a :b "b" & c] (list a b c))]
     (are [expr answer] (= expr answer)
          (f 0)        '(0)
          (g)          '(0)
@@ -26,25 +22,42 @@
     )))
 
 (deftest test-letfnk
-  (letfnk [(f [a :b "b" & c]
-             (list a b c))]
+  (letfnk [(f [a] (list a))
+           (g [:a 0] (list a))
+           (h [a :b "b"] (list a b))
+           (i [a :b "b" & c] (list a b c))]
     (are [expr answer] (= expr answer)
-         (f 0)        '(0 "b" ())
-         (f 0 1)      '(0 "b" (1))
-         (f 0 1 2)    '(0 "b" (1 2))
-         (f 0 :b 0)   '(0 0 ())
-         (f 0 :b 0 0) '(0 0 (0)))))
+         (f 0)        '(0)
+         (g)          '(0)
+         (g 1)        '(0)
+         (g :a 1)     '(1)
+         (h 0)        '(0 "b")
+         (h 0 :b 1)   '(0 1)
+         (i 0)        '(0 "b" ())
+         (i 0 1)      '(0 "b" (1))
+         (i 0 1 2)    '(0 "b" (1 2))
+         (i 0 :b 0)   '(0 0 ())
+         (i 0 :b 0 0) '(0 0 (0))
+    )))
 
-(defnk f-test [a :b "b" & c]
-  (list a b c))
+(defnk f-test [a] (list a))
+(defnk g-test [:a 0] (list a))
+(defnk h-test [a :b "b"] (list a b))
+(defnk i-test [a :b "b" & c] (list a b c))
 
 (deftest test-defnk
   (are [expr answer] (= expr answer)
-       (f-test 0)        '(0 "b" ())
-       (f-test 0 1)      '(0 "b" (1))
-       (f-test 0 1 2)    '(0 "b" (1 2))
-       (f-test 0 :b 0)   '(0 0 ())
-       (f-test 0 :b 0 0) '(0 0 (0))))
+       (f-test 0)        '(0)
+       (g-test)          '(0)
+       (g-test 1)        '(0)
+       (g-test :a 1)     '(1)
+       (h-test 0)        '(0 "b")
+       (h-test 0 :b 1)   '(0 1)
+       (i-test 0)        '(0 "b" ())
+       (i-test 0 1)      '(0 "b" (1))
+       (i-test 0 1 2)    '(0 "b" (1 2))
+       (i-test 0 :b 0)   '(0 0 ())
+       (i-test 0 :b 0 0) '(0 0 (0))))
 
 (defmulti fmulti-test
   (fn [t & _] (class t)))
