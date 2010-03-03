47b97745e5f00152c01b305393c0a73669ddf841
maoe <maoe.maoe@gmail.com>
2010-03-03 14:08:57 +0900

    add more tests

--- test/test.clj
+++ test/test.clj
@@ -3,14 +3,27 @@
   (:use clojure.test))
 
 (deftest test-fnk
-  (let [f (fnk [a :b "b" & c]
+  (let [f (fnk [a]
+            (list a))
+        g (fnk [:a 0]
+            (list a))
+        h (fnk [a :b "b"]
+            (list a b))
+        i (fnk [a :b "b" & c]
             (list a b c))]
     (are [expr answer] (= expr answer)
-         (f 0)        '(0 "b" ())
-         (f 0 1)      '(0 "b" (1))
-         (f 0 1 2)    '(0 "b" (1 2))
-         (f 0 :b 0)   '(0 0 ())
-         (f 0 :b 0 0) '(0 0 (0)))))
+         (f 0)        '(0)
+         (g)          '(0)
+         (g 1)        '(0)
+         (g :a 1)     '(1)
+         (h 0)        '(0 "b")
+         (h 0 :b 1)   '(0 1)
+         (i 0)        '(0 "b" ())
+         (i 0 1)      '(0 "b" (1))
+         (i 0 1 2)    '(0 "b" (1 2))
+         (i 0 :b 0)   '(0 0 ())
+         (i 0 :b 0 0) '(0 0 (0))
+    )))
 
 (deftest test-letfnk
   (letfnk [(f [a :b "b" & c]
